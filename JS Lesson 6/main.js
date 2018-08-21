5f3f0c609c868436708740de2bf25b8cf43f3d22
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 15:28:53 -0600

    6

--- JS Lesson 6/main.js
+++ JS Lesson 6/main.js
@@ -2,6 +2,8 @@ const newYearsResolutions = ['diet', 'work out', 'get more sleep'];
 
 const listItem = newYearsResolutions[0];
 newYearsResolutions[1] = 'Learn a new language';
+newYearsResolutions.push('volunteer more', 'get another dog');
+newYearsResolutions.pop();
 
 console.log(newYearsResolutions);
 console.log(listItem);

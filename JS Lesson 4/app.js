7cfab2392a6503ae0cd61d048c6f001723312b9b
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 12:15:52 -0600

    4

--- JS Lesson 4/app.js
+++ JS Lesson 4/app.js
@@ -1,5 +1,5 @@
-const takeOrder = (topping) => {
-  console.log(`Order: pizza topped with ${topping}`);
+const takeOrder = (topping, crustType) => {
+  console.log(`Order: ${crustType} pizza topped with ${topping}`);
 };
 
-takeOrder("tomatoes");
+takeOrder('mushrooms', "crispy crust");

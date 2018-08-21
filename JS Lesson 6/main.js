95e5137b1afdc2ee9e8df2bac846d301cac1936d
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 15:23:15 -0600

    3

--- JS Lesson 6/main.js
+++ JS Lesson 6/main.js
@@ -1,3 +1,8 @@
 const newYearsResolutions = ['diet', 'work out', 'get more sleep'];
 
+const listItem = newYearsResolutions[0];
+
 console.log(newYearsResolutions);
+console.log(listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);

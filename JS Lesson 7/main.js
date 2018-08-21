e62547029d2548947f8a18d587df36ea30560f3f
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 16:01:39 -0600

    5

--- JS Lesson 7/main.js
+++ JS Lesson 7/main.js
@@ -1,5 +1,11 @@
-let vacationSpots = ['Greece', 'Peru', 'Scotland'];
+let myPlaces = ['Greece', 'Peru', 'Scotland'];
+let friendPlaces = ['Ireland', 'Greece', 'China'];
 
-for (let vacationSpotIndex=(vacationSpots.length - 1); vacationSpotIndex>=0; vacationSpotIndex--) {
-  console.log(`I would love to visit ${vacationSpots[vacationSpotIndex]}`);
-};
+for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++) {
+  for (let friendPlacesIndex = 0; friendPlacesIndex < friendPlaces.length; friendPlacesIndex++) {
+    if(myPlaces[myPlacesIndex] === friendPlaces[friendPlacesIndex]) {
+      console.log(myPlaces[myPlacesIndex]);
+      console.log(friendPlaces[friendPlacesIndex]);
+    }
+  }
+}

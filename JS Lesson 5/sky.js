5ac62cafe9cf71e229500a5c5beace52952c333e
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 15:13:17 -0600

    5

--- JS Lesson 5/sky.js
+++ JS Lesson 5/sky.js
@@ -1,7 +1,11 @@
 const visibleLightWaves = () => {
   let lightWaves = 'Moonlight';
+  let region = 'The Arctic';
+  if (region === 'The Arctic') {
+    const lightWaves = 'Northern Lights';
+    console.log(lightWaves);  
+  }
   console.log(lightWaves);
 };
 
 visibleLightWaves();
-console.log(lightWaves);

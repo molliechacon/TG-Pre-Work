89e61a0562bed4180d9b7fb26d789f4fc395324f
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 12:42:16 -0600

    6

--- JS Lesson 4/app.js
+++ JS Lesson 4/app.js
@@ -1,15 +1,25 @@
 let orderCount = 0;
 
 const takeOrder = (topping, crustType) => {
+  orderCount++;
   console.log('Order: ' + crustType + ' pizza topped with ' + topping);
-	orderCount++;
-};
-
-const getSubTotal = (itemCount) => {
-  return(itemCount * 7.5);
 };
 
 takeOrder('mushroom', 'thin crust');
 takeOrder('spinach', 'whole wheat');
 takeOrder('pepperoni', 'brooklyn style');
+
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+};
+
+const getTax = (itemCount) => {
+  return getSubTotal(itemCount) * .06;
+};
+
+const getTotal = (param) => {
+  return getSubTotal(param) + getTax(param);
+}
+
 console.log(getSubTotal(orderCount));
+console.log(getTotal(orderCount));

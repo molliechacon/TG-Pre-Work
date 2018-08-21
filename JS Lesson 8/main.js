1a58074a6b757b820084b0a5de88c65aaec8600f
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 16:55:50 -0600

    2

--- JS Lesson 8/main.js
+++ JS Lesson 8/main.js
@@ -1,22 +1,9 @@
-let artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
-artists.forEach(function(artist) {
-  console.log(artist + ' is one of my favorite artists.');
+// Iterate over fruits below
+/* fruits.forEach(function(fruitType) {
+  console.log(`I want to eat a ${fruitType}.`);
 });
+*/
 
-let numbers = [1, 2, 3, 4, 5];
-
-let squareNumbers = numbers.map(function(number) {
-  return number * number;
-});
-
-console.log(squareNumbers);
-
-let things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
-
-let onlyNumbers = things.filter(function(thing) {
-  return typeof thing === 'number';
-});
-
-console.log(onlyNumbers);
-
+fruits.forEach(fruitType => console.log(`I want to eat a ${fruitType}.`));

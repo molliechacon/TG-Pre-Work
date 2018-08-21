7dbe8916a55bd5a2ce142688aab8f2087418a2db
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 15:33:24 -0600

    7

--- JS Lesson 6/main.js
+++ JS Lesson 6/main.js
@@ -1,12 +1,10 @@
-const newYearsResolutions = ['diet', 'work out', 'get more sleep'];
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
 
-const listItem = newYearsResolutions[0];
-newYearsResolutions[1] = 'Learn a new language';
-newYearsResolutions.push('volunteer more', 'get another dog');
-newYearsResolutions.pop();
+groceryList.shift();
+groceryList.unshift('popcorn');
+console.log(groceryList.slice(1, 4));
 
-console.log(newYearsResolutions);
-console.log(listItem);
-console.log(newYearsResolutions[2]);
-console.log(newYearsResolutions[3]);
-console.log(newYearsResolutions.length);
+console.log(groceryList);
+
+
+

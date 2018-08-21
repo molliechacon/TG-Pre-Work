dde5f2feec9b8b1526a2608df9b307b9d4996423
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 09:05:50 -0600

    3

--- JS Lesson 3/app.js
+++ JS Lesson 3/app.js
@@ -1,7 +1,16 @@
-let isSoccerFan = false;
+let wordCount = 800;
 
-if(isSoccerFan === true) {
-  console.log("Goal!");
+if (wordCount) {
+  console.log("Great! You've started your work!");
 } else {
-  console.log("No goal!");
-};
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = 'You either make yourself miserable or make yourself strong; the amount of work is the same';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}

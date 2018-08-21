64312b69836e2e0be0145e031646cba6767166da
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 09:00:28 -0600

    2

--- JS Lesson 3/app.js
+++ JS Lesson 3/app.js
@@ -1,12 +1,7 @@
-let userName = 'Mollie';
-let knowsJavaScript = true;
+let isSoccerFan = false;
 
-if (knowsJavaScript && userName) {
-  console.log('Great, ' + userName + '! Get ready to practice your JavaScript!');
-} else if (knowsJavaScript) {
-  console.log('Great! Get ready to practice your JavaScript!');
-} else if (userName) {
-  console.log('Great, ' + userName + '! Get ready to learn something new!');
+if(isSoccerFan === true) {
+  console.log("Goal!");
 } else {
-  console.log('Great! Get ready to learn something new!');
-}
+  console.log("No goal!");
+};

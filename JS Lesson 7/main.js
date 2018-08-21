e913119ec14f6e8ab68178fdbcfac327bf7a1af0
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 16:06:53 -0600

    6

--- JS Lesson 7/main.js
+++ JS Lesson 7/main.js
@@ -1,11 +1,9 @@
-let myPlaces = ['Greece', 'Peru', 'Scotland'];
-let friendPlaces = ['Ireland', 'Greece', 'China'];
+let cards = ['Diamond', 'Spade', 'Heart', 'Club'];
+let currentCard = 'Heart';
 
-for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++) {
-  for (let friendPlacesIndex = 0; friendPlacesIndex < friendPlaces.length; friendPlacesIndex++) {
-    if(myPlaces[myPlacesIndex] === friendPlaces[friendPlacesIndex]) {
-      console.log(myPlaces[myPlacesIndex]);
-      console.log(friendPlaces[friendPlacesIndex]);
-    }
-  }
+while (currentCard !== 'Spade') {
+  console.log(currentCard);
+  currentCard = cards[Math.floor(Math.random() *4)];
 }
+
+console.log('found a spade');

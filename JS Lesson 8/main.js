41414080408cf23be11aaf03ed71d9fa22d03dd7
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 17:14:40 -0600

    3

--- JS Lesson 8/main.js
+++ JS Lesson 8/main.js
@@ -1,9 +1,24 @@
-let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-// Iterate over fruits below
-/* fruits.forEach(function(fruitType) {
-  console.log(`I want to eat a ${fruitType}.`);
-});
-*/
+// Create the secretMessage array below
+//let secretMessage = animals.map(function(animalType) {
+//  return animalType[0];
+//});
 
-fruits.forEach(fruitType => console.log(`I want to eat a ${fruitType}.`));
+//Refactored to an arrow function
+let secretMessage = animals.map(animalType => animalType[0]);
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+//let smallNumbers = bigNumbers.map(function(bigNum) {
+//  return bigNum / 100;
+//});
+
+//Refactored to an arrow function
+let smallNumbers = bigNumbers.map(bigNum => bigNum / 100);
+
+console.log(smallNumbers);
+

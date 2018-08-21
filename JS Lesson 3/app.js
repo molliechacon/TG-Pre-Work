138faccccb038a821b76f21920f0e36bbe701771
mollie chacon <mollie.mosley@gmail.com>
2018-08-21 09:09:40 -0600

    5

--- JS Lesson 3/app.js
+++ JS Lesson 3/app.js
@@ -1,16 +1,7 @@
-let wordCount = 800;
+let hungerLevel = 10;
 
-if (wordCount) {
-  console.log("Great! You've started your work!");
+if(hungerLevel > 7) {
+  console.log("Time to eat!");
 } else {
-  console.log('Better get to work!');
-}
-
-
-let favoritePhrase = 'You either make yourself miserable or make yourself strong; the amount of work is the same';
-
-if (!favoritePhrase) {
-  console.log("This string doesn't seem to be empty.");
-} else {
-  console.log('This string is definitely empty.');
-}
+  console.log("We can eat leter!");
+};

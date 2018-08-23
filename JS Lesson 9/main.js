c86f92d6f2139bce6aee5dd9ae4d711955e4efa2
mollie chacon <mollie.mosley@gmail.com>
2018-08-23 13:12:59 -0600

    14

--- JS Lesson 9/main.js
+++ JS Lesson 9/main.js
@@ -2,16 +2,23 @@ let person = {
   _name: 'Lu Xun',
   _age: 137,
   
-  set age(newAge) {
-    if (typeof newAge === 'number') {
-      this._age = newAge;
-      console.log("New age is valid");
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
     }
     else {
       console.log('Invalid input');
       return 'Invalid input';
     }
+  },
+  
+  get age() {
+    console.log(`${this._name} is ${this._age} years old.`);
+    return this._age;
   }
+
 };
 
 person.age = 39;
+
+console.log(person.age);

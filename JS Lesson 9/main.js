2a20b5ce61469f547df470f7c3ec3785d222e53a
mollie chacon <mollie.mosley@gmail.com>
2018-08-23 13:02:45 -0600

    13

--- JS Lesson 9/main.js
+++ JS Lesson 9/main.js
@@ -5,11 +5,13 @@ let person = {
   set age(newAge) {
     if (typeof newAge === 'number') {
       this._age = newAge;
+      console.log("New age is valid");
     }
     else {
       console.log('Invalid input');
       return 'Invalid input';
     }
   }
-
 };
+
+person.age = 39;

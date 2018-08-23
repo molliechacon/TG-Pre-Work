9d8316772f7f09ea26f63f67ca44236965d56294
mollie chacon <mollie.mosley@gmail.com>
2018-08-23 13:00:05 -0600

    12

--- JS Lesson 9/main.js
+++ JS Lesson 9/main.js
@@ -1,37 +1,15 @@
-let day = "Thursday";
-let alarm;
-
 let person = {
-  name: 'Mollie',
-  age: 35,
-  weekendAlarm: 'No alarms needed',
-  weekAlarm: 'Alarm set to 7am',
-  sayHello: () => {
-    // COULD NOT GET THIS.NAME TO WORK!!! SOMEHOW, PERSON.NAME WORKS JUST FINE.
-    return(`Hello, my name is ${this.name}`);
-  },
-  sayGoodbye() {
-    return("Goodbye!");
+  _name: 'Lu Xun',
+  _age: 137,
+  
+  set age(newAge) {
+    if (typeof newAge === 'number') {
+      this._age = newAge;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
   }
+
 };
-
-let friend = {
-  name: 'Sundi'
-};
-
-friend.sayHello = person.sayHello;
-
-person.hobbies = ['Kickboxing', 'Reading'];
-person.hobbies = ['Knitting'];
-
-if (day === "Saturday" || day === "Sunday") {
-  alarm = 'weekendAlarm';
-} else {
-  alarm = 'weekAlarm';
-}
-
-console.log(person[alarm]);
-console.log(person.hobbies);
-console.log(person.sayHello());
-console.log(person.sayGoodbye());
-console.log(friend.sayHello());

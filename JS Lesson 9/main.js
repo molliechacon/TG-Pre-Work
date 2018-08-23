add2d3e3e95bbfcf532a0bd9e76cf9c7930eaa9b
mollie chacon <mollie.mosley@gmail.com>
2018-08-23 12:46:28 -0600

    11

--- JS Lesson 9/main.js
+++ JS Lesson 9/main.js
@@ -8,12 +8,18 @@ let person = {
   weekAlarm: 'Alarm set to 7am',
   sayHello: () => {
     // COULD NOT GET THIS.NAME TO WORK!!! SOMEHOW, PERSON.NAME WORKS JUST FINE.
-    return(`Hello, my name is ${person.name}`);
+    return(`Hello, my name is ${this.name}`);
   },
   sayGoodbye() {
     return("Goodbye!");
   }
 };
+
+let friend = {
+  name: 'Sundi'
+};
+
+friend.sayHello = person.sayHello;
 
 person.hobbies = ['Kickboxing', 'Reading'];
 person.hobbies = ['Knitting'];
@@ -28,3 +34,4 @@ console.log(person[alarm]);
 console.log(person.hobbies);
 console.log(person.sayHello());
 console.log(person.sayGoodbye());
+console.log(friend.sayHello());

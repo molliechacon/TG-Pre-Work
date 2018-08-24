95d7af53b144e24c2e25abf3bb9fde9a85bd83ca
mollie chacon <mollie.mosley@gmail.com>
2018-08-23 19:55:48 -0600

    10

--- JS Lesson 12/airplane.js
+++ JS Lesson 12/airplane.js
@@ -1,19 +1,16 @@
-
-
 export let availableAirplanes = [
-{
-  name: 'AeroJet',
-  fuelCapacity: 800, 
-  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
-  maxSpeed: 1200, 
-  minSpeed: 300
- }, 
- {name: 'SkyJet',
-  fuelCapacity: 500,
-  availableStaff: ['pilots', 'flightAttendants'],
-  maxSpeed: 800, 
-  minSpeed: 200
- }
+{name: 'AeroJet',
+ fuelCapacity: 800,
+ availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+ maxSpeed: 1200,
+ minSpeed: 300
+}, 
+{name: 'SkyJet',
+ fuelCapacity: 500,
+ availableStaff: ['pilots', 'flightAttendants'],
+ maxSpeed: 800,
+ minSpeed: 200
+}
 ];
 
 export let flightRequirements = {
@@ -21,19 +18,19 @@ export let flightRequirements = {
   requiredSpeedRange: 700
 };
 
- export function meetsStaffRequirements(availableStaff, requiredStaff) {
+export function meetsStaffRequirements(availableStaff, requiredStaff) {
   if (availableStaff.length >= requiredStaff) {
     return true;
   } else {
     return false;
   }
-}
+};
 
 export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
   let range = maxSpeed - minSpeed;
   if (range > requiredSpeedRange) {
     return true;
-  } else {
+    } else {
     return false;
   }
-}
+};
--- JS Lesson 12/missionControl.js
+++ JS Lesson 12/missionControl.js
@@ -1,4 +1,4 @@
-import { availableAirplanes, flightRequirements, meetsStaffRequirements } from './airplane';
+import { availableAirplanes, flightRequirements, meetsStaffRequirements, meetsSpeedRangeRequirements } from './airplane';
 
 function displayFuelCapacity() {
 	availableAirplanes.forEach(function(element) {
@@ -12,5 +12,13 @@ function displayStaffStatus() {
   });
 }
 
+function displaySpeedRangeStatus() {
+  availableAirplanes.forEach(function(element) {
+		console.log(element.name + ' meets speed range requirements: ' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange)) ;
+  });
+}
+
 displayFuelCapacity();
 displayStaffStatus();
+displaySpeedRangeStatus();
+

aaae6740cbaadfe0211049d532222064eb83f65a
mollie chacon <mollie.mosley@gmail.com>
2018-08-23 19:24:38 -0600

    7

--- JS Lesson 12/airplane.js
+++ JS Lesson 12/airplane.js
@@ -1,14 +1,27 @@
-let Airplane = {};
 
-Airplane.availableAirplanes = [
-  {
-    name: "AeroJet",
-    fuelCapacity: 800
-  },
-  {
-    name: "SkyJet",
-    fuelCapacity: 500
-  }
+
+let availableAirplanes = [
+{
+  name: 'AeroJet',
+  fuelCapacity: 800, 
+  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators']
+ }, 
+ {name: 'SkyJet',
+  fuelCapacity: 500,
+  availableStaff: ['pilots', 'flightAttendants']
+ }
 ];
 
-export default Airplane;
+let flightRequirements = {
+  requiredStaff: 4
+};
+
+function meetsStaffRequirements(availableStaff, requiredStaff) {
+  if (availableStaff.length >= requiredStaff) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export { availableAirplanes, flightRequirements, meetsStaffRequirements};

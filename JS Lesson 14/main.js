3815493ce9e9d8d3f39ce3a23a9db511b8465264
mollie chacon <mollie.mosley@gmail.com>
2018-08-24 12:35:25 -0600

    9

--- JS Lesson 14/main.js
+++ JS Lesson 14/main.js
@@ -12,7 +12,15 @@ const shortenUrl = () => {
   const urlToShorten = inputField.value;
   const data = JSON.stringify({destination: urlToShorten});
   fetch(url, {method: 'POST', headers: { 'Content-type': 'application/json',
-  'apikey': apiKey}, body: data});
+  'apikey': apiKey}, body: data})
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error ('Request failed!');
+  }, networkError => {
+    console.log(networkError.message);
+  });
 }
 
 // Clear page and call AJAX functions
